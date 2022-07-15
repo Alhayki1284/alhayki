b5314a30179e84ebe747eb2308c774cce8eb651d
Alhayki1284 <108177193+Alhayki1284@users.noreply.github.com>
2022-07-16 01:21:26 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,99 @@
+
+let currenMusic = 0;
+
+const music = document.querySelector('#audio');
+
+const seekBar = document.querySelector('.seek-bar');
+const songName = document.querySelector('.music-name');
+const artistName = document.querySelector('.artist-name');
+const disk = document.querySelector('.disk');
+const currentTime = document.querySelector('.current-time');
+const musicDuration = document.querySelector('.song-duration');
+const playBtn = document.querySelector('.play-btn');
+const forwardBtn = document.querySelector('.forward-btn');
+const backwardBtn = document.querySelector('.backward-btn');
+
+playBtn.addEventListener('click', () => {
+    if(playBtn.className.includes('pause')){
+        music.play();
+    } else{
+        music.pause();
+    }
+    playBtn.classList.toggle('pause');
+    disk.classList.toggle('play');
+})
+
+//setup music
+
+const setMusic = (i) => {
+    seekBar.value = 0; //set range slide value to 0;
+    let song = songs[i];
+    currenMusic = i;
+    music.src = song.path;
+
+    songName.innerHTML = song.name;
+    artistName.innerHTML = song.artist;
+    disk.style.backgroundImage = `url('${song.cover}')`;
+
+    currentTime.innerHTML = '00:00';
+    setTimeout(() => {
+        seekBar.max = music.duration;
+        musicDuration.innerHTML = formatTime(music.duration);
+    }, 300);
+}
+
+setMusic(0);
+
+// formating time in min and seconds format
+
+const formatTime = (time) => {
+    let min = Math.floor(time / 60);
+    if(min < 10){
+        min = `${min}`;
+    }
+    let sec = Math.floor(time % 60);
+    if(sec < 10){
+        sec = `0${sec}`;
+    }
+    return `${min} : ${sec}`;
+}
+
+//seek bar
+setInterval(() => {
+    seekBar.value = music.currentTime;
+    currentTime.innerHTML = formatTime(music.currentTime);
+    if(Math.floor(music.currenMusic) == Math.floor(seekBar.max)){
+        forwardBtn.click();
+    }
+}, 500)
+
+seekBar.addEventListener('change', () => {
+     music.currentTime = seekBar.value;
+})
+
+const playMusic = () => {
+    music.play();
+    playBtn.classList.remove('pause');
+    disk.classList.add('play');
+}
+
+//forward and backward button
+forwardBtn.addEventListener('click', () => {
+    if(currenMusic >= songs.length - 1){
+        currenMusic = 0;
+    } else{
+        currenMusic++;
+    }
+    setMusic(currenMusic);
+    playMusic();
+})
+
+backwardBtn.addEventListener('click', () => {
+    if(currenMusic <= 0){
+        currenMusic = songs.length - 1;
+    } else{
+        currenMusic--;
+    }
+    setMusic(currenMusic);
+    playMusic();
+})
---
+++ data.js
@@ -0,0 +1,44 @@
+let songs = [
+    {
+        name: 'المقدمة',
+        path: 'https://www.raed.net/download?fileid=84604',
+        artist: 'صالح الدرازي و الشيخ حسين الأكرف',
+        cover: 'media/cover.jpg'
+    },
+    {
+        name: 'الموشح',
+        path: 'https://www.raed.net/download?fileid=84605',
+        artist: 'صالح الدرازي و الشيخ حسين الأكرف',
+        cover: 'media/cover.jpg'
+    },
+    {
+        name: 'وسيلة لكل حوايجنا',
+        path: 'https://www.raed.net/download?fileid=84606',
+        artist: 'صالح الدرازي و الشيخ حسين الأكرف',
+        cover: 'media/cover.jpg'
+    },
+    {
+        name: 'أريد اسأل',
+        path: 'https://www.raed.net/download?fileid=84607',
+        artist: 'صالح الدرازي و الشيخ حسين الأكرف',
+        cover: 'media/cover.jpg'
+    },
+    {
+        name: 'يجوز اعتب',
+        path: 'https://www.raed.net/download?fileid=84608',
+        artist: 'صالح الدرازي و الشيخ حسين الأكرف',
+        cover: 'media/cover.jpg'
+    },
+    {
+        name: 'وقفات',
+        path: 'https://www.raed.net/download?fileid=84609',
+        artist: 'صالح الدرازي و الشيخ حسين الأكرف',
+        cover: 'media/cover.jpg'
+    },
+    {
+        name: 'نعي',
+        path: 'https://www.raed.net/download?fileid=84594',
+        artist: 'صالح الدرازي و الشيخ حسين الأكرف',
+        cover: 'media/cover.jpg'
+    }
+]
